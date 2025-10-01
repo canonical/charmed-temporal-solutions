492200422e90d2e82af657590cb38602bbd62738
arjun <arjun.malik@canonical.com>
2025-10-01 06:17:58 -0600

    charmed-temporal: add Terraform solution
    - Added applications.tf, integrations.tf, cos_configuration.tf,
      outputs.tf, variables.tf, versions.tf, main.tf

---
+++ terraform/charmed-temporal/outputs.tf
@@ -0,0 +1,64 @@
+
+output "applications" {
+  description = "All charm modules which make up this product module."
+  value = {
+    postgresql     = module.postgresql
+    temporal       = module.temporal_server
+    temporal_ui    = module.temporal_ui
+    temporal_admin = module.temporal_admin
+  }
+}
+
+# Requirers exposed by Temporal Server
+output "frontend_certificates_requirer" {
+  description = "Map containing the app_name and the requires of the TLS requirer charm."
+  value = {
+    app_name = local.app_names.temporal
+    requires = local.requires.temporal.frontend_certificates
+  }
+}
+
+output "server_nginx_route_requirer" {
+  description = "Map containing the app_name and the requires of the nginx route requirer charm (server)."
+  value = {
+    app_name = local.app_names.temporal
+    requires = local.requires.temporal.nginx_route
+  }
+}
+
+# Requirers exposed by Temporal UI
+output "ui_nginx_route_requirer" {
+  description = "Map containing the app_name and the requires of the nginx route requirer charm (UI)."
+  value = {
+    app_name = local.app_names.temporal_ui
+    requires = local.requires.temporal_ui.nginx_route
+  }
+}
+
+# OpenFGA integration (from Temporal Server)
+output "openfga_requirer" {
+  description = "Map containing the app_name and the requires of the OpenFGA requirer charm."
+  value = {
+    app_name = local.app_names.temporal
+    requires = local.requires.temporal.openfga
+  }
+}
+
+# Grafana Agent (only if cos_configuration = true)
+output "grafana_agent_k8s" {
+  description = "Map containing the app_name, provides, and requires endpoints of the grafana-agent-k8s charm used."
+  value = local.cos_enabled ? {
+    app_name  = local.grafana_agent_app_name
+    provides  = try(module.grafana_agent[0].provides, {})
+    requires  = try(module.grafana_agent[0].requires, {})
+  } : {}
+}
+
+# S3 Integrator (from Temporal Server)
+output "s3_integrator_requirer" {
+  description = "Map containing the app_name and the requires of the s3 integrator requirer charm."
+  value = {
+    app_name = local.app_names.temporal
+    requires = local.requires.temporal.s3_parameters
+  }
+}
